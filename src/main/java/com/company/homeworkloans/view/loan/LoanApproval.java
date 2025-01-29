3c5b34c2221053a4baf3c8d34a47edc98975efc2
Matvey Kondratev <kondratev.matvei@yandex.ru>
2025-01-29 21:21:31 +0300

    complete 3rd task

--- src/main/java/com/company/homeworkloans/view/loan/LoanApproval.java
+++ src/main/java/com/company/homeworkloans/view/loan/LoanApproval.java
@@ -1,11 +1,20 @@
 package com.company.homeworkloans.view.loan;
 
 import com.company.homeworkloans.entity.Loan;
+import com.company.homeworkloans.entity.LoanStatus;
 import com.company.homeworkloans.view.main.MainView;
+import com.vaadin.flow.component.ClickEvent;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.data.renderer.Renderer;
 import com.vaadin.flow.router.Route;
+import io.jmix.core.DataManager;
+import io.jmix.flowui.Notifications;
 import io.jmix.flowui.component.grid.DataGrid;
+import io.jmix.flowui.kit.component.button.JmixButton;
+import io.jmix.flowui.model.CollectionLoader;
+import io.jmix.flowui.model.InstanceContainer;
 import io.jmix.flowui.view.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -17,5 +26,55 @@ import java.time.LocalTime;
 @LookupComponent("loansDataGrid")
 @DialogMode(width = "64em")
 public class LoanApproval extends StandardListView<Loan> {
+    @ViewComponent
+    private DataGrid<Loan> loansDataGrid;
+    @Autowired
+    private DataManager dataManager;
+    @Autowired
+    private Notifications notifications;
+    @ViewComponent
+    private CollectionLoader<Loan> loansDl;
+
+    @Subscribe(id = "approveBtn", subject = "clickListener")
+    public void onApproveBtnClick(final ClickEvent<JmixButton> event) {
+        Loan singleSelectedItem = loansDataGrid.getSingleSelectedItem();
+
+        if(singleSelectedItem == null) {
+            notifications.create("Выберете запись").show();
+            return;
+        }
+
+        singleSelectedItem.setStatus(LoanStatus.APPROVED);
+        dataManager.save(singleSelectedItem);
+        notifications.create("Success", "Approved")
+                .withType(Notifications.Type.SUCCESS)
+                .withPosition(Notification.Position.TOP_END)
+                .withDuration(3000)
+                .show();
+        loansDl.load();
+
+    }
+
+    @Subscribe(id = "rejectBtn", subject = "clickListener")
+    public void onRejectBtnClick(final ClickEvent<JmixButton> event) {
+        Loan singleSelectedItem = loansDataGrid.getSingleSelectedItem();
+
+        if(singleSelectedItem == null) {
+            notifications.create("Выберете запись").show();
+            return;
+        }
+
+        singleSelectedItem.setStatus(LoanStatus.REJECTED);
+        dataManager.save(singleSelectedItem);
+        notifications.create("Success", "Rejected")
+                .withType(Notifications.Type.SUCCESS)
+                .withPosition(Notification.Position.TOP_END)
+                .withDuration(3000)
+                .show();
+        loansDl.load();
+
+    }
+
+
 
 }
